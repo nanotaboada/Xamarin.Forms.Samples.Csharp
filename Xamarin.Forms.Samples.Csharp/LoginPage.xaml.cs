7dfc70910b302591e0675226fe5676c49e5f018b
Nano Taboada <nanotaboada@msn.com>
2017-01-11 14:31:13 -0300

    Added Login page.

---
+++ Xamarin.Forms.Samples.Csharp/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.Forms.Samples.Csharp
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
+		{
+			Content = new StackLayout()
+			{
+				Spacing = 20,
+				Padding = 20,
+				VerticalOptions = LayoutOptions.Center,
+				Children =
+				{
+					new Image { Source = "codevel-512.png" },
+					new Entry { Placeholder = "Enter your email" },
+					new Entry { Placeholder = "Password", IsPassword = true },
+					new Button { Text = "Login", TextColor = Color.White, BackgroundColor = Color.FromHex("#377EB8") }
+				}
+			};
+		}
+	}
+}
